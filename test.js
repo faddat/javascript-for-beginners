50a6079e8aa7a6fc219a8d351be668bbbe220181
Jacob Gadikian <jacobgadikian@gmail.com>
2019-11-06 17:26:14 -0500

    added comments

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
+// the first alert
 alert("Hello, world!");
+
+// the second alert
+alert("Hello, world!");
+
+// move the image from top to bottom
+
+// make the image invisible
+
+// make the image visible
